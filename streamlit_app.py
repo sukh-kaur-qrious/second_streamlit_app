dc73ace12fdf3818e8988fc457e0bce84f6a77eb
sukh-kaur-qrious <116595301+sukh-kaur-qrious@users.noreply.github.com>
2022-11-04 15:32:44 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.title('Zena\'s Amazing Athleisure Catalog')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select color_or_style from catalog")
+my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 # put the dafta into a dataframe
 df = pandas.DataFrame(my_catalog)
@@ -20,7 +20,7 @@ option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list
 # We'll build the image caption now, since we can
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 # use the option selected to go back and get all the info from the database
-my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog")
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
 df2 = my_cur.fetchone()
 streamlit.image(
 df2[0],
